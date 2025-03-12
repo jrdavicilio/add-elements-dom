eb1f7f579510710f830f44e28b30a6791632aab5
David Fernández <davidferal26@gmail.com>
2025-03-12 20:47:01 +0100

    PROYECTO FINAL

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
  // Aquí tu código
+
+ const button = document.getElementById("agregar")
+ const lista = document.getElementById("lista")
+
+
+ 
+ button.addEventListener("click", function(){
+    const peliculas = prompt("¿Cuáles son tus películas favoritas");
+    if (peliculas) {
+        const li = document.createElement("li");
+        lista.appendChild(li);
+        li.textContent = peliculas;
+        li.classList.add("nuevaPeli")
+    }
+    })
